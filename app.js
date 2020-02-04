37c478f484f756bd4424567b582d3ace90f29fd6
Ryan <ryan@Ryans-MacBook-Pro-4.local>
2020-02-04 09:29:22 -0600

    Final

---
+++ app.js
@@ -0,0 +1,57 @@
+
+
+let fruits = ['Banana', 'orange', 'Apple'];
+
+function printFruits() {
+  
+  let favFruit = fruits[2];
+  let leastFav = fruits[0]
+  console.log(fruits[0]);
+
+  function printFavFruit() {
+    
+    console.log(favFruit);
+  }
+
+  function printLeastFavFruit() {
+    console.log(leastFav);
+    
+  }
+  printFavFruit();
+  printLeastFavFruit();
+  
+}
+
+printFruits();
+
+
+someFunc();
+function someFunc() {
+  console.log ('this is text')
+}
+
+
+let fun = function declare (){
+ alert('alert this')
+}
+
+console.log(fun);
+
+
+
+
+
+// function Fruit(){
+//   let favFruit = fruits[2];
+//   console.log(fruits[0]);
+
+//   function favFruit(){
+//     console.log(favFruit);
+//   }
+//   favFruit();
+// };
+
+
+// Fruit();
+
+// console.log('End script');
